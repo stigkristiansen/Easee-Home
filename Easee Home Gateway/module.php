8201eee1a8fd1abb98f9ce666ca7d335622083b0
stigkristiansen <stigkristiansen@outlook.com>
2021-09-24 17:12:17 +0200

    .

--- Easee Home Gateway/module.php
+++ Easee Home Gateway/module.php
@@ -49,42 +49,55 @@ class EaseeHomeGateway extends IPSModule
 
 		$function = strtolower($request->Function);
 		$childId =  strtolower($request->ChildId);
+
+		$username = $this->ReadPropertyString('Username') ;
+		$password = $this->ReadPropertyString('Password') ;
 		
 		switch($function) {
 			case 'getproducts':
-				$this->GetProducts($childId);
+				$this->GetProducts($childId, $username, $password);
 				break;
 			case 'getcharger':
 				if(!isset($request->ChargerId)) {
 					throw new Exception(sprintf('HandleAsyncRequest: Invalid formated request. Key "ChargerId" is missing. The request was "%s"', $Request));
 				}
 				
-				$this->GetCharger($childId, $request->ChargerId);
+				$this->GetCharger($childId, $request->ChargerId, $username, $password);
+				break;
+			case 'getequalizerstate':
+				if(!isset($request->EqualizerId)) {
+					throw new Exception(sprintf('HandleAsyncRequest: Invalid formated request. Key "EqualizerId" is missing. The request was "%s"', $Request));
+				}
+				
+				$this->GetEqualizerState($childId, $request->EqualizerId, $username, $password);
 				break;
 			default:
 				throw new Exception(sprintf('HandleAsyncRequest failed. Unknown function "%s"', $function));
 		}
 	}
 
-	private function GetEqualizerState(string $EqualizerId) {
-		$easee = new Easee();
+	private function GetEqualizerState(string $EqualizerId, string $Username, string $Password) {
+		$easee = new Easee($Username, $Password);
 		$easee->DisableSSL();
+		
 		$products = $easee->GetEqualizerState($EqualizerId);
 		
 		$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('Easee REST API returned "%s" for GetEqualizerState()', json_encode($products)), 0);
 	}
 
-	private function GetProducts(string $ChildId) {
-		$easee = new Easee();
+	private function GetProducts(string $ChildId, string $Username, string $Password) {
+		$easee = new Easee($Username, $Password);
 		$easee->DisableSSL();
+		
 		$products = $easee->GetProducts();
 		
 		$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('Easee REST API returned "%s" for GetProducts()', json_encode($products)), 0);
 	}
 
-	private function GetCharger(string $ChildId, $ChargerId) {
-		$easee = new Easee();
+	private function GetCharger(string $ChildId, $ChargerId, string $Username, string $Password) {
+		$easee = new Easee($Username, $Password);
 		$easee->DisableSSL();
+		
 		$products = $easee->GetCharger($ChargerId);
 		
 		$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('Easee REST API returned "%s" for GetCharger()', json_encode($products)), 0);
