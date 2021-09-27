bdd23e0c8d763597d4649ca96c1b742b139116ec
stigkristiansen <stigkristiansen@outlook.com>
2021-09-27 16:39:46 +0200

    Update module.php

--- Easee Home Gateway/module.php
+++ Easee Home Gateway/module.php
@@ -149,7 +149,7 @@ class EaseeHomeGateway extends IPSModule
 
 			$now = new DateTime('now');
 			$diff = $token->Expires->diff($now);
-			$millisec = (((($diff->days*24+$diff->h)*60+$diff->i)*60+$diff->s)-5*60)*1000; // Timeout 5 minutes before the token is expirering
+			$millisec = (((($diff->days*24+$diff->h)*60+$diff->i)*60+$diff->s)-5*60)*1000; 
 
 			$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('Now: %s', $now->format('Y-m-d H:i:s')), 0);
 			$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('Expire: %s', $token->Expires->format('Y-m-d H:i:s')), 0);
