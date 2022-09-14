eae86a16f90b636cbd4a4ce2a845452225c99c2f
stigkristiansen <stigkristiansen@outlook.com>
2022-09-14 16:36:59 +0200

    Update module.php

--- Easee Home Charger/module.php
+++ Easee Home Charger/module.php
@@ -336,7 +336,7 @@ include __DIR__ . "/../libs/traits.php";
 											$this->SendDebug(IPS_GetName($this->InstanceID), sprintf('This was the last call to GetCommandState for now. Updated TicksTable is: %s', json_encode($ticksTable)), 0);
 											
 											$this->SendDebug(IPS_GetName($this->InstanceID), 'Quering for new charger status in 5s', 0);
-											$this->SetTimerInterval('EaseeChargerRefresh' . (string)$this->InstanceID, 5000); 
+											$this->SetTimerInterval('EaseeChargerRefresh' . (string)$this->InstanceID,5000); 
 										}
 
 										break;
