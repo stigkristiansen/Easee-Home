0663fe8fd1dacfc87ca84ec3a98d64686251bc3c
stigkristiansen <stigkristiansen@outlook.com>
2025-05-14 19:43:42 +0200

    Update module.php

--- Easee Home Gateway/module.php
+++ Easee Home Gateway/module.php
@@ -181,7 +181,7 @@ class EaseeHomeGateway extends IPSModule
 							'Result' => $decodedInfo['arguments']
 						];
 
-						$chagerId=$decodedInfo['arguments']['mid'];
+						$chargerId=$decodedInfo['arguments']['mid'];
 
 						$this->SendDataToChildren(json_encode(["DataID" => "{47508B62-3B4E-67BE-0F29-0B82A2C62B58}", "ChildId" => $chargerId, "Buffer" => $forwardingData]));
 						break;
