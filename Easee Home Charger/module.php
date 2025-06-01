c10e4ceac2610a4b25d2d05c55491e5678981f6e
stigkristiansen <stigkristiansen@outlook.com>
2025-06-01 15:50:24 +0200

    Update module.php

--- Easee Home Charger/module.php
+++ Easee Home Charger/module.php
@@ -485,7 +485,7 @@ class EaseeHomeCharger extends IPSModule {
 				$this->SendDebug(__FUNCTION__, sprintf('Observation Id %d is an Id that corresponds to Ident "%s"', $Data->id, $change['Ident']), 0);
 				$oldObservation = $this->GetReceivedObservation($change['Ident']);
 				if($oldObservation!==false) {
-					if($oldObservation['Timestamp']>$change['Timestamp']) {
+					if($oldObservation['timestamp']>$change['timestamp']) {
 						return; // Newer observation has already been handeled
 					}
 				}
