dd5e5bd46cbb467dd5f8a90c74b1c0d4fa61b8f7
stigkristiansen <stigkristiansen@outlook.com>
2025-06-04 13:29:35 +0200

    .

--- libs/easee.php
+++ libs/easee.php
@@ -378,7 +378,7 @@ class Easee {
             
             //$url = self::ENDPOINT . '/api/chargers/' . $ChargerId .'/state';
 
-            $url = self::ENDPOINT . '/state' . '/{' . $ChargerId .'}/observations?{30,42,109}';
+            $url = self::ENDPOINT . '/state' . '/' . $ChargerId .'/observations?{30,42,109}';
 
             $result = self::EvaluateResult(self::request('get', $url), $url);
             
